b9861debca2d3fc4b334e4512b61881a1b761fbc
doddamanisandeep <98304236+doddamanisandeep@users.noreply.github.com>
2024-05-27 21:02:24 +0530

    Create Student_count.py

---
+++ Python Scripting/Scripts/Student_count.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+score=input("Please Enter the score")
+
+if score >= 80 and score <=100:
+  print ("Grade A/Distinction")
+elif score <= 79 and score >= 50:
+  print ("Grade B/First Class"
+elif scroe <=49 and score >= 35:
+  print ("Just Passed")
+else:
+  print ("Failed")
+  
+
