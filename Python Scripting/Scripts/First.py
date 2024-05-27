b21dba7b685a43d13a467d2e8a8c4b3601761c94
doddamanisandeep <98304236+doddamanisandeep@users.noreply.github.com>
2024-05-27 11:32:52 +0530

    Update First.py

--- Python Scripting/Scripts/First.py
+++ Python Scripting/Scripts/First.py
@@ -1,1 +1,20 @@
-#
+#!/usr/bin/python3
+
+# Comment Section
+
+#MLC
+'''
+Hi im line1 Comment section
+Hi im line2 Comment section
+Hi im line3 Comment section
+'''
+
+"""
+Hi im line4 Comment section
+Hi im line5 Comment section
+Hi im line6 Comment section
+"""
+
+#Dinefine variable
+var1=100
+print(var1)
