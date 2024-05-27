5f2b5643114f22d4810953c15749dcfa6694037c
doddamanisandeep <98304236+doddamanisandeep@users.noreply.github.com>
2024-05-27 19:35:03 +0530

    Create 2nd.py

---
+++ Python Scripting/Scripts/2nd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Working with Data types
+
+str1="Hi Team welcome to Python Scripting"
+lis1=[10,20,40,50,100]
+tup1={100,200,400,600}
+dict1={"Subject" : "Pyhton", "Mode" : "Online"}
+
+# Accessing data
+print("My Str1 data is " +str1)
+print("My lis1 data is " +str((lis1))
+print("My lis1 fst data is +str(lis1[0])
+print("My tup data is " +str(tup1)
+print("My tup1 last data is " + str(tup1[-1])
+print("My Dict1 Subject valuve is " + dict1["Subejct"]) 
+print("My dict1 Mode value is " + dict1["mode"]
