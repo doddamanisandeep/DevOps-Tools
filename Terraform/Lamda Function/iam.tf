b484bb5125a1f1bec25c17f282159b2d13be4385
doddamanisandeep <98304236+doddamanisandeep@users.noreply.github.com>
2024-06-13 23:14:36 +0530

    Create iam.tf

---
+++ Terraform/Lamda Function/iam.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "lambda_role" {
+  name = var.lambda_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+      },
+    ],
+  })
+
+  tags = {
+    Name = "lambda-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
