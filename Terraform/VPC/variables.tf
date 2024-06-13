3b0aa292fb8eb76d39f821a71cc12e3193f0f0d6
doddamanisandeep <98304236+doddamanisandeep@users.noreply.github.com>
2024-06-13 23:11:12 +0530

    Create variables.tf

---
+++ Terraform/VPC/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
